88ee4496050fa9814a530b2a044d148421fc2e28
Aliyu Yahaya <royalalite@gmail.com>
2025-09-17 20:01:46 +0200

    Add project files.

---
+++ ST10440112-PROG6212-CCMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10440112_PROG6212_CCMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
